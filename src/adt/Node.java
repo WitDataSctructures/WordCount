fa731217f04a0f867d912ed3684aa84c7dc3931a
Darrien <kennedyd3@wit.edu>
2016-03-30 08:54:28 -0400

    Added to Node.java

--- src/adt/Node.java
+++ src/adt/Node.java
@@ -1,5 +1,49 @@
 package adt;
 
 public class Node {
-
+	
+	private Node nextNode;
+	private String key;
+	private int value;
+	
+	//Constructors
+	public Node(){
+		nextNode = null;
+		key = null;
+		value = 0;
+	}
+	
+	public Node(Node nxt, String ky, int val){
+		nextNode = nxt;
+		key = ky;
+		value = val;
+	}
+	
+	
+	//Setters
+	public void setNextNode(Node nxt){
+		this.nextNode = nxt;
+	}
+	
+	public void setKey(String ky){
+		this.key = ky;
+	}
+	
+	public void setValue(int val){
+		this.value = val;
+	}
+	
+	
+	//Getters
+	public Node getNextNode(){
+		return this.nextNode;
+	}
+	
+	public String getKey(){
+		return this.key;
+	}
+	
+	public int getValue(){
+		return this.value;
+	}
 }
